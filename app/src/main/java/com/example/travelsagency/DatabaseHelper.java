5ba08f730b885c12c6ecdeb869477f42b499007c
htmtmx <cesar.hpp96@hotmail.com>
2022-05-15 23:18:41 -0500

    01 Add diagram DB travelsAgency and initial sentence create table TRAVELS

--- app/src/main/java/com/example/travelsagency/DatabaseHelper.java
+++ app/src/main/java/com/example/travelsagency/DatabaseHelper.java
@@ -21,6 +21,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE if not exists USERS(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name text, email text, telephone text, rfc text, role text, password text)");
+        //Create table for Travels
+        db.execSQL("CREATE TABLE if not exists TRAVELS(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name text, description text, destination text, location_exit text, location_arrive text, start_travel datetime, hour_exit timestamp, end_travel datetime, quantity intger, rating float, price float, FOREIGN KEY (agency_id_fk) REFERENCES AGENCIES (agency_id), FOREIGN KEY (vehicle_id_fk) REFERENCES VEHICLES (vehicle_id))");
+
+
         //db.execSQL("CREATE TABLE if not exists USERS(id int primary key, name text, email text, telephone text, rfc text, role text, password text)");
     }
 
